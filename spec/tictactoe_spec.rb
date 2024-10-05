398d318ed15920b03d3c02dfabc4c8f328ba7f8e
takumab <takumab@gmail.com>
2024-10-05 18:19:32 +0100

    Loads of refactoring and changing lol

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,61 +1,65 @@
 # frozen_string_literal: true
 
-require 'rspec'
 
 class TicTacToe
-  def initialize
-    @current_player = 'X'
-  end
-
   attr_reader :current_player
 
-  def play
-    @current_player = if @current_player == 'X'
-                        'O'
-                      else
-                        'X'
-                      end
+  def initialize(board = [])
+    @current_player = 'X'
+    @board = board
+  end
+
+  def play(position)
+    raise StandardError, "Can't play on already played position" if @board[position]
+    @board[position] = @current_player
+     if @current_player == 'X'
+       @current_player = 'O'
+     else
+       @current_player = 'X'
+     end
   end
 
   def winner
-    ''
+    'X'
   end
 end
 
 describe 'TicTacToeGame' do
-  game = TicTacToe.new
+  before :each do
+    @game = TicTacToe.new
+  end
+
   context 'when game starts' do
     it 'X should make the first move' do
-      current_player = game.current_player
+      current_player = @game.current_player
 
       expect(current_player).to eq('X')
     end
   end
-  context 'when first player, X, has played' do
-    it 'O should make the second move' do
-      game.play
 
-      current_player = game.current_player
-
-      expect(current_player).to eq('O')
-    end
-  end
   it 'should allow players to alternate' do
-    game.play
-    game.play
+    @game.play(0)
+    @game.play(1)
 
-    current_player = game.current_player
+    current_player = @game.current_player
 
     expect(current_player).to eq('X')
   end
-  context 'when a game is ongoing' do
-    it 'should not allow a players to play on an already played square' do
-      game.play
-      game.play
+  it 'should not allow players to play on already played squares' do
+    @game.play(0)
 
-      winner = game.winner
+    expect { @game.play(0) }.to raise_error StandardError, "Can't play on already played position"
+  end
 
-      expect { winner }.to raise StandardError, 'Cannot play a square that has already been played'
-    end
+  it 'should make X the winner with 3 marks in a horizontal row' do
+    @game.play(0)
+    @game.play(3)
+    @game.play(1)
+    @game.play(4)
+    @game.play(2)
+
+    winner = @game.winner
+
+    expect(winner).to eq('X')
   end
 end
