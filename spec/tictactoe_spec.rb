88bde64f99eb7c963bb9054dbf435290af3da162
takumab <takumab@gmail.com>
2024-10-06 14:25:44 +0100

    should make O the winner with 3 marks in second horizontal row

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -110,4 +110,17 @@ describe 'TicTacToeGame' do
 
     expect(winner).to eq('X')
   end
+
+  it 'should make O the winner with 3 marks in second horizontal row' do
+    @game.play(0)
+    @game.play(3)
+    @game.play(1)
+    @game.play(4)
+    @game.play(6)
+    @game.play(5)
+
+    winner = @game.winner
+
+    expect(winner).to eq('O')
+  end
 end
